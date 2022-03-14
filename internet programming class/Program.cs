207a212ac14bb28d6cb60ab3f9049e1e08812013
b1095047 <b10956047@mail.npust.edu.tw>
2022-03-14 10:12:32 +0800

    作業1

--- internet programming class/Program.cs
+++ internet programming class/Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
 
-namespace B10956047
+namespace _220314
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Console.WriteLine("分支測試!");
+            Console.WriteLine("請輸入一個數字:");
+            var input = Console.ReadLine();
+            int num = int.Parse(input);
 
-            //var input = Console.ReadLine();
-            //Console.WriteLine("你輸入的是:" + input);
-
-            double meal_cost = 100;
-            int real_meal_cost = (int)meal_cost;
-            int tip_percent = 15;
-            int tax_percent = 8;
-            String test1 = $"用餐費用:{meal_cost}元";
-            String test2 = $"服務費:{tip_percent}元";
-            String test3 = $"稅率:{tax_percent}元";
-            Console.WriteLine(test1);
-            Console.WriteLine(test2);
-            Console.WriteLine(test3);
-            int total_cost = real_meal_cost + tip_percent + tax_percent;
-            String test4 = $"用餐總費用:{total_cost}元";
-            Console.WriteLine(test4);
+            if (num % 2 != 0)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (num >= 2 && num <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if (num >= 6 && num <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
         }
     }
 }
-
