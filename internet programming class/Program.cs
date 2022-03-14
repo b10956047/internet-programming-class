c3a699eb5b0037e13a05e871670563bda4d3a385
b1095047 <b10956047@mail.npust.edu.tw>
2022-03-14 11:17:24 +0800

    陣列(座號)

--- internet programming class/Program.cs
+++ internet programming class/Program.cs
@@ -6,25 +6,48 @@ namespace _220314
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("請輸入一個數字:");
+            String[,] s = new string[,] { { "1", "Jack", "85", "70", "65" }, { "2", "Mary", "65", "67", "62" }, { "3", "Tom", "100", "93", "91" }, { "4", "Grace", "98", "25", "50" }, { "5", "Alice", "83", "82", "87" } };
+
+
+            Console.WriteLine("請輸入座號(1-5):");
             var input = Console.ReadLine();
             int num = int.Parse(input);
 
-            if (num % 2 != 0)
+            switch(input)
             {
-                Console.WriteLine("Weird");
-            }
-            else if (num >= 2 && num <= 5)
-            {
-                Console.WriteLine("Not Weird");
-            }
-            else if (num >= 6 && num <= 20)
-            {
-                Console.WriteLine("Weird");
-            }
-            else
-            {
-                Console.WriteLine("Not Weird");
+                case "1":
+                    Console.WriteLine($"1.姓　　名:{s[num - 1, 1]}");
+                    Console.WriteLine($"2.電腦概論:{s[num - 1, 2]}");
+                    Console.WriteLine($"3.程式設計:{s[num - 1, 3]}");
+                    Console.WriteLine($"4.動畫設計:{s[num - 1, 4]}");
+                    break;
+                case "2":
+                    Console.WriteLine($"1.姓　　名:{s[num - 1, 1]}");
+                    Console.WriteLine($"2.電腦概論:{s[num - 1, 2]}");
+                    Console.WriteLine($"3.程式設計:{s[num - 1, 3]}");
+                    Console.WriteLine($"4.動畫設計:{s[num - 1, 4]}");
+                    break;
+                case "3":
+                    Console.WriteLine($"1.姓　　名:{s[num - 1, 1]}");
+                    Console.WriteLine($"2.電腦概論:{s[num - 1, 2]}");
+                    Console.WriteLine($"3.程式設計:{s[num - 1, 3]}");
+                    Console.WriteLine($"4.動畫設計:{s[num - 1, 4]}");
+                    break;
+                case "4":
+                    Console.WriteLine($"1.姓　　名:{s[num - 1, 1]}");
+                    Console.WriteLine($"2.電腦概論:{s[num - 1, 2]}");
+                    Console.WriteLine($"3.程式設計:{s[num - 1, 3]}");
+                    Console.WriteLine($"4.動畫設計:{s[num - 1, 4]}");
+                    break;
+                case "5":
+                    Console.WriteLine($"1.姓　　名:{s[num - 1, 1]}");
+                    Console.WriteLine($"2.電腦概論:{s[num - 1, 2]}");
+                    Console.WriteLine($"3.程式設計:{s[num - 1, 3]}");
+                    Console.WriteLine($"4.動畫設計:{s[num - 1, 4]}");
+                    break;
+                default:
+                    Console.WriteLine("座號超出範圍");
+                    break;
             }
         }
     }
