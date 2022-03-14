0b2f36dcc5f058a09607c7679126475a81972669
b1095047 <b10956047@mail.npust.edu.tw>
2022-03-14 10:11:30 +0800

    作業1

--- internet programming class/Program.cs
+++ internet programming class/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
-namespace internet_programming_class
+namespace B10956047
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("分支測試!");
+
+            //var input = Console.ReadLine();
+            //Console.WriteLine("你輸入的是:" + input);
+
+            double meal_cost = 100;
+            int real_meal_cost = (int)meal_cost;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            String test1 = $"用餐費用:{meal_cost}元";
+            String test2 = $"服務費:{tip_percent}元";
+            String test3 = $"稅率:{tax_percent}元";
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+            Console.WriteLine(test3);
+            int total_cost = real_meal_cost + tip_percent + tax_percent;
+            String test4 = $"用餐總費用:{total_cost}元";
+            Console.WriteLine(test4);
         }
     }
 }
+
