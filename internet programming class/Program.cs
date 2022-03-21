b80e5fe2e9c30120efc75acd16c8186c7ae81b92
user <user@DESKTOP-6JBEF5K>
2022-03-21 10:28:50 +0800

    練習 - HttpClient 物件

--- internet programming class/Program.cs
+++ internet programming class/Program.cs
@@ -6,7 +6,33 @@ namespace internet_programming_class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("123");
+            //Car myCar = new Car();
+            //myCar.ShowInfo();
+            //myCar.ChangeColor();
+            //myCar.ShoweInfo();
+
+            //myCar carA = new CarA();
+            //carA.OpenDoor();
+            await MyHttpClientAsync();
+        }
+
+        static private async Task MyHttpClientAsync()
+        {
+            static readonly HttpClient client = new HttpClient();
+            try	
+            {
+                string url = "http://www.contoso.com/";
+                HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+            }
         }
     }
 }
